eeabdca4d415db3cb61b8a4cf2404334555c8ede
Chavan <pramod.chavan@thermofisher.com>
2019-12-18 00:23:19 +0530

    Updating the main java file for first time

--- app/src/main/java/com/example/noteai/MainActivity.java
+++ app/src/main/java/com/example/noteai/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.noteai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.noteai.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
     }
+
+    public void openLoginActivity() {
+        Intent intent=new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
