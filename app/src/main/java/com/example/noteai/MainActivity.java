4bad0e0ccb4c3dedb7fc35b5448cec4fb6f94fc8
Chavan <pramod.chavan@thermofisher.com>
2019-12-19 00:20:07 +0530

    Initiated Firebase Backend Mechanism

--- app/src/main/java/com/example/noteai/MainActivity.java
+++ app/src/main/java/com/example/noteai/MainActivity.java
@@ -9,19 +9,23 @@ import android.widget.Button;
 
 import com.example.noteai.ui.login.LoginActivity;
 
+
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button button1;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1=(Button)findViewById(R.id.button_login);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openLoginActivity();
             }
         });
+
+
     }
 
     public void openLoginActivity() {
